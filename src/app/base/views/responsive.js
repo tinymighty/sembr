3484c118d5b6994726da1f5ccf172356b00aee8a
Andru Vallance <andru@tinymighty.com>
2014-03-14 20:25:24 +0100

    Updated to use new navigation templates.

--- src/app/base/views/responsive.js
+++ src/app/base/views/responsive.js
@@ -1,35 +1,39 @@
-define(['sembr', 'jquery', 'backbone', 'marionette', 'base/views/header', 
-'semantic-ui',
-'hbs!../templates/responsive'],
-function (sembr, $, Backbone, Marionette, HeaderView, SemanticUI, template) {
-    //ItemView provides some default rendering logic
-    return Backbone.Marionette.Layout.extend({
-        template:template,
+define([
+	'require', 'sembr', 'backbone', 'marionette', 'base/views/navigation', 'semantic-ui',
+	'hbs!base/templates/responsive', 
+	'css!base/resources/css/reset', 'css!/../libs/semantic-ui/css/semantic', 'css!base/resources/css/commonstyles.css', 'css!base/resources/css/fonts/museo/stylesheet.css'
+], function (
+	require, sembr, Backbone, Marionette, NavView, SemanticUI, 
+	template
+) {
+	//ItemView provides some default rendering logic
+	return Backbone.Marionette.Layout.extend({
+		template:template,
 
-        attributes: {'data-role': "layout"},
+		attributes: {'data-role': "layout"},
 
-        events:{
-            'click a': 'preventDefault'
-        },
-        
-        regions:{
-            header: '[data-role="header"]',
-            content: '[data-role="content"]',
-            footer: '[data-role="footer"]'
-        },
+		events:{
+			'click a': 'preventDefault'
+		},
+		
+		regions:{
+			header: '[data-role="header"]',
+			content: '[data-role="content"]',
+			footer: '[data-role="footer"]'
+		},
 
-        initialize: function(){
-            this.headerView = new HeaderView();
-        },
+		initialize: function(){
+			this.navView = new NavView();
+		},
 
-        onRender: function(){
-            this.header.show( this.headerView );
-        },
+		onRender: function(){
+			this.header.show( this.navView );
+		},
 
-        preventDefault: function($ev){
-            $ev.preventDefault();
-            return false;
-        }
+		preventDefault: function($ev){
+			//$ev.preventDefault();
+			//return false;
+		}
 
-    });
+	});
 });
